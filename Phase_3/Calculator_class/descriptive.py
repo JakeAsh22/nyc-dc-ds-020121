c258df042fe9bfb0107230e15a1fbef71549d6ba
JakeAsh22 <jacob.ash1998@gmail.com>
2021-03-16 11:46:08 -0400

    finish calc

--- Phase_3/Calculator_class/descriptive.py
+++ Phase_3/Calculator_class/descriptive.py
@@ -8,22 +8,66 @@ Created on Wed Sep  4 14:25:52 2019
 import math
 
 
-class Calculator:
+class Calculator():
     def __init__(self, data):
-        self.data = data;
+        self.data = data
+        self.length = self.__get_length()
         self.mean = self.__calc_mean()
+        self.median = self.__calc_median()
+        self.variance = self.__calc_variance()
+        self.stand_dev = self.__calc_stand_dev()
 
+    def __get_length(self):
+        return len(self.data)
 
     def __calc_mean(self):
-        mean = sum(self.data)/len(self.data)
+        # # print(type(self.data))
+        # # print(type(self.length))
+        # d = self.data
+        # print(d)
+        # sum_d = sum(d)
+        mean = sum(self.data)/self.length
         return mean
 
+    def __calc_median(self):
+        sorted_data = self.data
+        sort_data = sorted(sorted_data)
+        if len(self.data) % 2 == 0:
+            median1 = sort_data[self.length//2]
+            median2 = sort_data[self.length//2 - 1]
+            median = (median1 + median2)/2
+        else:
+            median = sort_data[self.length//2]
+        return median
+
+    def __calc_variance(self):
+        m = self.mean
+        n = self.length
+        deviations = [(x - m) ** 2 for x in self.data]
+        print(deviations)
+        variance = sum(deviations) / (len(self.data)-1)
+        print(variance ** 0.5)
+        return variance
+
+        # avg = sum(lst) / len(lst)
+        # var = sum((x-avg)**2 for x in lst) / len(lst)
+
+    def __calc_stand_dev(self):
+        return self.variance ** 0.5
+
     def add_data(self, data):
-        self.data.append(data)
+        self.data.extend(data)
+        self.length = len(self.data)
         self.mean = self.__calc_mean()
+        self.median = self.__calc_median()
+        self.variance = self.__calc_variance()
+        self.stand_dev = self.__calc_stand_dev()
 
-    pass        
-
-
-
-
+    def remove_data(self, data):
+        self.data = [x for x in self.data if x not in data]
+        self.length = len(self.data)
+        self.mean = self.__calc_mean()
+        self.median = self.__calc_median()
+        self.variance = self.__calc_variance()
+        self.stand_dev = self.__calc_stand_dev()
+    pass
--- Phase_3/Calculator_class/test_calculator.py
+++ Phase_3/Calculator_class/test_calculator.py
@@ -9,9 +9,12 @@ import pytest
 from descriptive import Calculator
 
 print('starting tests')
+
+
 def test_mean():
     data = [2, 10, 15, 20, 45, 55, 80]
     instance = Calculator(data)
+    print(instance.data)
     actual_mean = instance.mean
     expected_mean = 32.42857
     message = "Expected value: {0}, Actual value: {1}".format(expected_mean,
@@ -22,19 +25,22 @@ def test_mean():
 def test_std():
     data = [2, 10, 15, 20, 45, 55, 80]
     instance = Calculator(data)
-    actual_std = instance.mean
+    actual_std = instance.stand_dev
     expected_std = 28.33641
     message = "Expected STD: {0}, Actual value: {1}".format(expected_std,
-                                                              actual_std)
+                                                            actual_std)
     assert instance.stand_dev == pytest.approx(28.33641), message
+
 
 def test_order():
     data = [2, 20, 45, 15, 10, 55, 80]
     instance = Calculator(data)
-    expected_median =20
+    expected_median = 20
     actual_median = instance.median
-    message = "Expected value: {0}, Actual value: {1}, did you order the ".format(expected_median, actual_median)
+    message = "Expected value: {0}, Actual value: {1}, did you order the ".format(
+        expected_median, actual_median)
     assert instance.median == 20, message
+
 
 def test_add_data():
     data = [2, 20, 45, 15, 10, 55, 80]
@@ -43,8 +49,9 @@ def test_add_data():
     actual_mean = instance.mean
     expected_mean = 35.875
     message = "called add_data() and expected mean: {0}, but got value: {1}".format(expected_mean,
-                                                              actual_mean)
+                                                                                    actual_mean)
     assert instance.mean == pytest.approx(expected_mean), message
+
 
 def test_remove_data():
     data = [2, 20, 45, 15, 10, 55, 80]
@@ -53,5 +60,5 @@ def test_remove_data():
     actual_mean = instance.mean
     expected_mean = 24.5
     message = "called remove_data() and expected mean: {0}, but got value: {1}".format(expected_mean,
-                                                              actual_mean)
+                                                                                       actual_mean)
     assert instance.mean == pytest.approx(expected_mean), message
